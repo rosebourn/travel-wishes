03c0623b0f8b2a735f2597ccc378f380711d1b0d
mianak78 <rosebourn@gmail.com>
2020-02-25 20:35:18 -0700

    start of app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        This is my project
+      </div>
+    );
+  }
+}
+
+export default App;
