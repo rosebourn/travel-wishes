b987b942350d9306f9a2eb15c4d2501247714f0c
mianak78 <rosebourn@gmail.com>
2020-03-07 10:14:24 -0700

    map api working

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -24,7 +24,7 @@ class Login extends Component {
 
   UNSAFE_componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
-      this.props.history.push("/dashboard"); // push user to dashboard when they login
+      this.props.history.push("/map"); // push user to dashboard when they login
     }
 
     if (nextProps.errors) {
--- client/src/components/layout/Map.js
+++ client/src/components/layout/Map.js
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
-// import { Link } from "react-router-dom";
 import ReactMapGL from "react-map-gl";
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { connect } from "mongoose";
 
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+// const TOKEN = "pk.eyJ1Ijoicm9zZWJvdXJuIiwiYSI6ImNrNm8wa2F0dzE2a3kzbm5xNWQ1NGpvMTUifQ.zZfvNREvPJxDYp3RLujCDg"
+const TOKEN = process.env.REACT_APP_MAPBOX_KEY
 const Map = () => {
   const [viewport, setViewport] = useState({
           width: "100vw",
           height: "100vh",
-          latitude: 45.4211,
-          longitude: -75.6903,
-          zoom: 8
+          latitude: 47.6062,
+          longitude: -122.3321,
+          zoom: 2
       });
 
       return (
           <div>
               <ReactMapGL 
               {...viewport}
+              mapboxApiAccessToken={TOKEN}
+              mapStyle="mapbox://styles/rosebourn/ck6nza16t18ct1iqbrjn5st2u"
+              onViewportChange={viewport => {
+                setViewport(viewport);
+              }}
               >
               </ReactMapGL>
           </div>
@@ -24,23 +30,3 @@ const Map = () => {
   }
 
   export default Map;
-
-  
-
-// const [viewport, setViewport] = useState({
-    //     width: "100vw",
-    //     height: "100vh",
-    //     latitude: 45.4211,
-    //     longitude: -75.6903,
-    //     zoom: 8
-    //   });
-    
-    //   return (
-    //   <div>
-    //     <ReactMapGL
-    //       {...viewport}
-    
-    //       >
-    //     </ReactMapGL>
-    //   </div>
-    //   );
